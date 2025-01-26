d74ad7e320c16cb7149819f81aff9816f05eb208
Nico Furtner <nicofurtner@gmail.com>
2025-01-26 14:48:58 +0100

    test

---
+++ sql-scripts/initdb.sql
@@ -0,0 +1,16 @@
+-- Tabelle 'users'
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Tabelle 'investment'
+CREATE TABLE IF NOT EXISTS investment (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    investment_type VARCHAR(255),
+    amount DECIMAL(15,2),
+    investment_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
